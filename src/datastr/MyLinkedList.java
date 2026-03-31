dd3a31729238c272a681328274b8751bba4207f5
s24raksagne <rakstinaagneta@gmail.com>
2026-03-31 11:46:13 +0300

    Edit: finished MyLinkedList class

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -55,7 +55,19 @@ public class MyLinkedList<Ttype> {
 		}
 	}
 	
-	
+	public void print() throws Exception {
+		if(isEmpty()) {
+			throw new Exception("Saraksta nav elementu, lidz ar to nevar neko izprintet");
+		}
+		
+		MyNode<Ttype> currentNode = firstNode;
+		
+		while(currentNode !=  null) {
+			System.out.print(currentNode.getElement() + " ");
+			currentNode = currentNode.getNextNode();
+		}
+		System.out.println();
+	}
 	
 	
 	
